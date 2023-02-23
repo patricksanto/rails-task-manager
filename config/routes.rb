10c137fa427298ec24e48a898e3ee74607b58579
Patrick Santos <patrick.sg.santos@gmail.com>
2023-02-23 16:08:34 -0300

    first app in rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # SHOW  ALL TASKS
+  get 'tasks', to: 'tasks#index'
+
+  # POST THE NEW TASK
+  post 'tasks', to: 'tasks#create'
+
+  # NEW TASK FORM
+  get 'tasks/new', to: 'tasks#new'
+
+  # SHOW A SINGLE ONE
+  get 'tasks/:id', to: 'tasks#show', as: '/task'
+
+  # form para editar
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # post the infos of edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+
 end
